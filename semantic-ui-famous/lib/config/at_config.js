667d55d3cc44d063133cb5dd132c7015cebc1ae6
Splendido <ing.luca.mussi@gmail.com>
2014-09-21 19:26:33 +0200

    restored correct configurations

---
+++ semantic-ui-famous/lib/config/at_config.js
@@ -0,0 +1,33 @@
+//Routes
+AccountsTemplates.configureRoute('changePwd');
+AccountsTemplates.configureRoute('enrollAccount');
+AccountsTemplates.configureRoute('forgotPwd');
+AccountsTemplates.configureRoute('resetPwd');
+AccountsTemplates.configureRoute('signIn');
+AccountsTemplates.configureRoute('signUp');
+AccountsTemplates.configureRoute('verifyEmail');
+
+// Options
+AccountsTemplates.configure({
+    showForgotPasswordLink: true,
+    overrideLoginErrors: true,
+    enablePasswordChange: true,
+    sendVerificationEmail: true,
+/*
+    confirmPassword: true,
+    continuousValidation: false,
+    displayFormLabels: true,
+    forbidClientAccountCreation: false,
+    formValidationFeedback: true,
+    homeRoutePath: '/',
+    showAddRemoveServices: false,
+    showPlaceholders: true,
+*/
+    negativeValidation: true,
+    positiveValidation:true,
+    negativeFeedback: false,
+    positiveFeedback:true,
+});
+
+// Initialization
+AccountsTemplates.init();
