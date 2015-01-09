2b1d4bc6dba5d819c1463416840aba7c36771f3e
Kevin O'Hagan <kevinohagan@gmail.com>
2015-01-09 17:06:40 +0100

    added polymer example repo

---
+++ polymer/lib/config/accounts_t9n.js
@@ -0,0 +1,1 @@
+T9n.setLanguage('en');
---
+++ polymer/lib/config/at_config.js
@@ -0,0 +1,32 @@
+//Routes
+AccountsTemplates.configureRoute('changePwd');
+AccountsTemplates.configureRoute('enrollAccount');
+AccountsTemplates.configureRoute('forgotPwd');
+AccountsTemplates.configureRoute('resetPwd');
+AccountsTemplates.configureRoute('signIn');
+AccountsTemplates.configureRoute('signUp');
+AccountsTemplates.configureRoute('verifyEmail');
+
+// Options
+AccountsTemplates.configure({
+    //defaultLayout: 'emptyLayout',
+    showForgotPasswordLink: true,
+    overrideLoginErrors: true,
+    enablePasswordChange: true,
+    sendVerificationEmail: false,
+
+    //enforceEmailVerification: true,
+    //confirmPassword: true,
+    //continuousValidation: false,
+    //displayFormLabels: true,
+    //forbidClientAccountCreation: false,
+    //formValidationFeedback: true,
+    //homeRoutePath: '/',
+    //showAddRemoveServices: false,
+    //showPlaceholders: true,
+
+    negativeValidation: true,
+    positiveValidation:true,
+    negativeFeedback: false,
+    positiveFeedback:true,
+});
