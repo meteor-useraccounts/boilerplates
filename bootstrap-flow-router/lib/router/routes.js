977de03f24b77bb1f74d3254ba35bd28c3b7a9ba
Luca Mussi <ing.luca.mussi@gmail.com>
2015-08-25 08:15:10 +0200

    bootstrap-flow-router

---
+++ bootstrap-flow-router/lib/router/routes.js
@@ -0,0 +1,42 @@
+
+FlowRouter.route('/', {
+  name: "home",
+  action: function(params, queryParams) {
+    BlazeLayout.render('masterLayout', {
+      footer: "footer",
+      main: "home",
+      nav: "nav",
+    });
+  }
+});
+
+FlowRouter.route('/private', {
+  name: "private",
+  triggersEnter: [AccountsTemplates.ensureSignedIn],
+  action: function(params, queryParams) {
+    BlazeLayout.render('masterLayout', {
+      footer: "footer",
+      main: "private",
+      nav: "nav",
+    });
+  }
+});
+
+FlowRouter.notFound = {
+  action: function() {
+    BlazeLayout.render('masterLayout', {
+      footer: "footer",
+      main: "pageNotFound",
+      nav: "nav",
+    });
+  }
+};
+
+
+//Routes
+AccountsTemplates.configureRoute('changePwd');
+AccountsTemplates.configureRoute('forgotPwd');
+AccountsTemplates.configureRoute('resetPwd');
+AccountsTemplates.configureRoute('signIn');
+AccountsTemplates.configureRoute('signUp');
+AccountsTemplates.configureRoute('verifyEmail');
---
+++ bootstrap-flow-router/server/lib/config/accounts.js
@@ -0,0 +1,53 @@
+// Set up login services
+Meteor.startup(function() {
+  // Add Facebook configuration entry
+  /*
+  ServiceConfiguration.configurations.update(
+    { service: "facebook" },
+    { $set: {
+        appId: "XXXXXXXXXXXXXXX",
+        secret: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+      }
+    },
+    { upsert: true }
+  );
+  */
+
+  // Add GitHub configuration entry
+  /*
+  ServiceConfiguration.configurations.update(
+    { service: "github" },
+    { $set: {
+        clientId: "XXXXXXXXXXXXXXXXXXXX",
+        secret: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+      }
+    },
+    { upsert: true }
+  );
+  */
+
+  // Add Google configuration entry
+  ServiceConfiguration.configurations.update(
+    { service: "google" },
+    { $set: {
+        clientId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        client_email: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        secret: "XXXXXXXXXXXXXXXXXXXXXXXX"
+      }
+    },
+    { upsert: true }
+  );
+
+  // Add Linkedin configuration entry
+  /*
+  ServiceConfiguration.configurations.update(
+    { service: "linkedin" },
+    { $set: {
+        clientId: "XXXXXXXXXXXXXX",
+        secret: "XXXXXXXXXXXXXXXX"
+      }
+    },
+    { upsert: true }
+  );
+  */
+});
