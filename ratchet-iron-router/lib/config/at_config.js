7d5e683bf40cf4df32082123e31bcdd87515529f
Luca Mussi <ing.luca.mussi@gmail.com>
2015-10-13 22:25:49 +0200

    added ratchet boilerplate

---
+++ ratchet-iron-router/lib/config/at_config.js
@@ -0,0 +1,27 @@
+// Options
+AccountsTemplates.configure({
+    // defaultLayout: 'emptyLayout',
+    showForgotPasswordLink: true,
+    overrideLoginErrors: true,
+    enablePasswordChange: true,
+
+  // sendVerificationEmail: true,
+  // enforceEmailVerification: true,
+  //confirmPassword: true,
+  //continuousValidation: false,
+  //displayFormLabels: true,
+  //forbidClientAccountCreation: true,
+  //formValidationFeedback: true,
+  //homeRoutePath: '/',
+  //showAddRemoveServices: false,
+  //showPlaceholders: true,
+
+  negativeValidation: true,
+  positiveValidation: true,
+  negativeFeedback: false,
+  positiveFeedback: true,
+
+  // Privacy Policy and Terms of Use
+  //privacyUrl: 'privacy',
+  //termsUrl: 'terms-of-use',
+});
